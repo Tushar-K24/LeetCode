76c4baf446f5ca2e684a9818d133e86ddcdb30f2
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2022-01-10 21:41:14 +0530

    Time: 69 ms (5.46%), Space: 14.4 MB (22.51%) - LeetHub

---
+++ 67-add-binary/67-add-binary.py
@@ -0,0 +1,19 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a=a[::-1]
+        b=b[::-1]
+        n = max(len(a), len(b))
+        a+="0"*(n-len(a))
+        b+="0"*(n-len(b))
+        print(a,b)
+        carry=0
+        ans=""
+        for i in range(n):
+            x,y = int(a[i]), int(b[i])
+            s = x+y+carry
+            carry=s//2
+            ans+=str(s%2)
+        if carry:
+            ans+=str(carry)
+        ans=ans[::-1]
+        return ans
