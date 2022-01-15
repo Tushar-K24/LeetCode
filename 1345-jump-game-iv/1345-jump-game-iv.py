975c1eb1983cd7030cffce942d89b245ccd53ecf
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2022-01-16 00:37:54 +0530

    Time: 1768 ms (5.08%), Space: 33.1 MB (22.34%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -1,36 +1,35 @@
+from collections import deque
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
-        graph=[[] for i in range(len(arr))]
-        ele = {}
-        for idx,i in enumerate(arr):
-            if i not in ele.keys():
-                ele[i]=[]
-            ele[i].append(idx)
-        for i in range(len(arr)-1):
-            graph[i].append(i+1)
-            graph[i+1].append(i)
-        ###################################################    
-        #bfs
-        vals = [0 for i in range(len(arr))]
-        visited = vals[:]
-        visited_ = {}
-        queue = []
+        if len(arr)==1: return 0
+        graph = {}
+        for i in range(len(arr)):
+            if arr[i] not in graph.keys():
+                graph[arr[i]]=[]
+            graph[arr[i]].append(i)
+        queue = deque([])
         queue.append(0)
+        visited = [0 for i in range(len(arr))]
+        ans = visited[:]
         visited[0]=1
+        visited_ = {}
         while len(queue):
-            x = queue.pop(0)
-            for i in graph[x]:
-                if not visited[i]:
-                    visited[i]=1
-                    vals[i]=vals[x]+1
-                    queue.append(i)
+            x = queue.popleft()
             if arr[x] not in visited_.keys():
                 visited_[arr[x]]=1
-                for i in ele[arr[x]]:
+                for i in graph[arr[x]]:
                     if not visited[i]:
                         visited[i]=1
-                        vals[i]=vals[x]+1
+                        ans[i]=ans[x]+1
                         queue.append(i)
-                    
-        ###################################################
-        return vals[-1]
+            if x-1>=0:
+                if not visited[x-1]:
+                    visited[x-1]=1
+                    ans[x-1]=ans[x]+1
+                    queue.append(x-1)
+            if x+1<len(arr):
+                if not visited[x+1]:
+                    visited[x+1]=1
+                    ans[x+1]=ans[x]+1
+                    queue.append(x+1)
+        return ans[-1]
