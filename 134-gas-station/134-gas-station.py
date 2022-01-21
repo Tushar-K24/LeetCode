4bfd967339ecaf71e207bb2692ce4a237eab86b3
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2022-01-21 15:43:31 +0530

    Time: 809 ms (14.74%), Space: 18.7 MB (94.41%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -4,10 +4,9 @@ class Solution:
         for i in range(2*n):
             curr+=gas[i%n]-cost[i%n]
             if curr<0:
-                if i>=n: return -1
-                st = (i+1)%n
-                curr=0
+                st, curr = (i+1)%n, 0
             else:
-                if i%n==(st-1)%n: return st
+                if i%n==(st-1)%n: 
+                    return st
         return -1
         
