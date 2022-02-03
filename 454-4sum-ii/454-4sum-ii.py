6ca1965605a87cd68bf6a82371713484348a7e63
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2022-02-03 14:31:07 +0530

    Time: 1407 ms (17.97%), Space: 14.2 MB (98.30%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.py
+++ 454-4sum-ii/454-4sum-ii.py
@@ -1,16 +1,21 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
         
-        cnt, f34 = 0, {}
+        cnt, f12, f34 = 0, {}, {}
+        for i in nums1:
+            for j in nums2:
+                s = i+j
+                if s not in f12.keys():
+                    f12[s]=0
+                f12[s]+=1
+        
         for i in nums3:
             for j in nums4:
                 s = i+j
                 if s not in f34.keys():
                     f34[s]=0
                 f34[s]+=1
-        for i in nums1:
-            for j in nums2:
-                target = -(i+j)
-                if target in f34.keys():
-                    cnt += f34[target]
+        for s in f12.keys():
+            if -s in f34.keys():
+                cnt+= f12[s]*f34[-s]
         return cnt
