cc57629f9899a950a002eaf8ec7e781e930b8e29
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2022-02-12 03:27:39 +0530

    Time: 12760 ms (14.18%), Space: 18.8 MB (67.38%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.py
+++ 312-burst-balloons/312-burst-balloons.py
@@ -4,13 +4,10 @@ class Solution:
         def sol_dp(i,j):
             if i>j: return 0
             if dp[i][j]!=-1: return dp[i][j]
-            #print('for {} and {}'.format(i,j))
             l = 1 if i-1<0 else nums[i-1]
             r = 1 if j+1>=len(nums) else nums[j+1]    
             for k in range(i,j+1):
-                #print('for k: {}'.format(k))
                 temp = nums[k]*l*r + sol_dp(i,k-1) + sol_dp(k+1,j)
-                #print('temp: {}'.format(temp))
                 dp[i][j] = max(dp[i][j],temp)
             return dp[i][j]
         return sol_dp(0,len(nums)-1)
