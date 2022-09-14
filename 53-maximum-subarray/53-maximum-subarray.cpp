1e2a22b658357bb150fb1818696344337b9b70f9
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2022-09-14 11:43:35 +0530

    Time: 230 ms (18.28%), Space: 67.7 MB (90.50%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    int helper(vector<vector<int>> &dp,vector<int> &nums, int n, bool flag) {
-        if(n == 0) {
-            if(!flag) return INT_MIN;
-            return 0;
-        }
-        if(dp[n][flag]!=-1e9-7) return dp[n][flag];
-        if(flag) return dp[n][flag]=max(0,nums[n-1]+helper(dp,nums,n-1,true));
-        return dp[n][flag]=max(nums[n - 1] + helper(dp,nums, n - 1, true), helper(dp,nums, n - 1,false));
-    }
     int maxSubArray(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size()+1,vector<int>(2,-1e9-7));
-        return helper(dp,nums, nums.size(),false);
+        int curr_max = nums[0];
+        int running_sum = 0;
+        for(int i = 0; i<nums.size(); i++){
+            running_sum += nums[i];
+            curr_max = max(curr_max, running_sum);
+            if(running_sum < 0)
+                running_sum = 0;
+        }
+        return curr_max;
     }
 };
